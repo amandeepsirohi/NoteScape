1200381f45148b1e47833017c0363829b21e46b3
buzz_404 <nitkkree@gmail.com>
2023-10-20 11:41:23 +0530

    Added starting navbar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+const DocumentsPage = () => {
+    return ( <div>
+        This is a protected page
+    </div> );
+}
+ 
+export default DocumentsPage;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
+
+
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F">
+        <Navigation />
+        <main className="flex h-full overflow-y-auto">
+        {children}
+        </main>
+    </div>
+
+  );
+};
+
+export default MainLayout;
