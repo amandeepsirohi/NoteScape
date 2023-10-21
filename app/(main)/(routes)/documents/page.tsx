19b0e7d607ee28a1e5acad43bda6bda133ca61f5
buzz_404 <nitkkree@gmail.com>
2023-10-21 16:09:47 +0530

    Added logout fxnality

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 
 const DocumentsPage = () => {
@@ -8,7 +10,7 @@ const DocumentsPage = () => {
 
   return (
     <div className="h-full  flex flex-col items-center justify-center space-y-4">
-       <Image
+      <Image
         src="/empty.png"
         height="300"
         width="300"
@@ -25,6 +27,9 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s NoteScape
       </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" /> Create a note
+      </Button>
     </div>
   );
 };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { isResSent } from "next/dist/shared/lib/utils";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from './user-item';
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -111,7 +112,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem/>
         </div>
         <div className="mt-4">
           <p>documents</p>
