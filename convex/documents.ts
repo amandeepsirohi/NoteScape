5a2e2d7cbfe8f61b96a792bfee7fd3a9538e8df4
buzz_404 <nitkkree@gmail.com>
2023-10-23 12:06:23 +0530

    added emoji option

--- convex/documents.ts
+++ convex/documents.ts
@@ -284,3 +284,32 @@ export const update = mutation({
 
   }
 });
+
+export const removeIcon = mutation({
+  args :{id:v.id("documents")},
+  handler: async (ctx , args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not Authenticated");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if(!existingDocument)
+    {
+      throw new Error("Not found");
+    }
+
+    if(existingDocument.userId !== userId)
+    {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id , {
+      icon:undefined
+    });
+
+    return document;
+  }
+})
